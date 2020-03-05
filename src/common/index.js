05df2aff2f5e9b225c43439ab6e240c160d1bd27
Felix Green <work@felix.green>
2020-03-04 20:53:41 -0500

    clear out old site

--- src/common/index.js
+++
@@ -1,9 +0,0 @@
-export { themeGet as t } from './utils'
-
-export { default as Box } from './Box'
-export { default as Container } from './Container'
-export { default as Fade } from './Fade'
-export { default as HexagonalIcon } from './HexagonalIcon'
-export { default as HexagonalLink } from './HexagonalLink'
-export { default as Paragraph } from './Paragraph'
-export { default as Text } from './Text'
