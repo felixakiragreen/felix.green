d72ddad4cbda29f299f401a357fb020f2284708d
Felix Green <work@felix.green>
2020-03-08 17:15:21 -0400

    ♻️ update PlainTextWarning with Aside

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
   import Index from './wiki/index.svelte'
   import Hexagon from 'components/Hexagon.svelte'
   import FelixAkiraGreen from 'components/FelixAkiraGreen.svelte'
-  import PlainTextWarning from 'components/PlainTextWarning.svelte'
+  import Aside from 'components/Aside.svelte'
 </script>
 
 <svelte:head>
@@ -20,6 +20,19 @@
   `}
   id="top"
 >
-  <PlainTextWarning />
+  <Aside>
+    <p class="text-orange-300">
+    <span class="plain">
+      Welcome 🖖
+      <br />
+      Why does the text look like
+    </span>
+    <span class="text-gray-100">this</span><span class="plain">?
+      <br />
+      It's a font based on my handwriting called
+      <a href="projects#akira" class="plain text-green-300 hover:text-green-400">&#123;Akira&#125;</a>.
+    </span>
+  </p>
+  </Aside>
   <Index />
 </div>
