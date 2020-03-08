b36498ce9b91b011c339ac953066e9b90465f9c2
Felix Green <work@felix.green>
2020-03-08 01:31:55 -0500

    📝 write projects page

---
+++ src/routes/projects.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Projects from './wiki/Projects.svelte'
+</script>
+
+<svelte:head>
+  <title>projects ⬣ f⬡g</title>
+</svelte:head>
+
+<div
+  class={`
+    relative
+    grid
+    gap-4
+    mt-8
+    z-20
+  `}
+  id="top"
+>
+  <Projects />
+</div>
---
+++ src/routes/wiki/projects.svelte
@@ -0,0 +1,111 @@
+<script>
+  import { projects } from '@f*g/fumonicon'
+</script>
+
+<style>
+  section {
+    @apply grid;
+    @apply gap-4;
+  }
+
+  p {
+    @apply px-8;
+  }
+</style>
+
+<div class="h-64">
+  {@html projects.svg}
+</div>
+
+
+
+I have far more ideas than I could ever accomplish in one or even a dozen lifetimes.
+At first I thought it was a blessing and then I thought it was a curse.
+Now I've just accepted that it's my 🧠brain.
+The 🧠 be what it do.
+
+I have found success in collaboration.
+And the best part of working together
+is that different perspectives often make something better than a single one ever could do.
+
+If any of these are interesting to you,
+join me on github (where this is all open source)
+or reach out to me by email at work @ felix.green
+
+
+
+Akira
+  "A font based on my own handwriting."
+
+  This project started when I was a child. 
+  I was not satisfied with the efficiency of writing, so I learned Gregg shorthand.
+  But the problem was no one else could read it.
+  Step 1. Single stroke (began in 12004)
+    So I started modifying how I wrote all the letters so it only used a single stroke,
+    but still be quite legible to others.
+  Step 2. Symmetry & geometry (began in 12013)
+    Once I was the only person reading it, I experimented aesthetically with many different forms.
+    I doubled the number of letters that are mirror versions of each other.
+  Step 3. Futuristic (began in 12018)
+    Then I wanted to turn it into a font. It's currently on version 2.2.6-- as you can see it's got
+    lowercase, uppercase, numbers, essential punctuation.
+
+  Seeking type designers that like this direction
+
+Felix Akira Green
+  "Inventing myself"
+  
+  As a child, I explored the idea of changing my name,
+  eager to become something, someone else.
+  
+  It wasn't until university (2009-2013) that I embraced my birth name--
+  I couldn't choose a name for myself until I knew who I wanted to become.
+
+  That took me over 10 years. Which ended up being good, since my personality, 
+  or more specifically, my prefrontal cortex, was still developing.
+
+  Step 0. Discover myself (began in 11991)
+  Step 1. Choose my name (on 9 September 12018)
+  Step 2. Launched this website (on 9 March 12020)
+  Step 3. Next update (expected 9 March 12021)
+
+Fumonicons (title of step 1)
+  "Hexagons. Hexagons everywhere."
+  
+  This project is still in its infancy.
+  And it's a story for another time.
+
+  Step 1. hexagonal icons (began 12019)
+  Step 2. futurisitc interfaces with rich and complex geometric and vector grid systems.
+
+  Seeking designers & developers with a fanatical affinity for hexagons.
+
+A Solarpunk Story About Shapes
+  "Yeah, I need a title"
+
+  A science fiction set in base reality, which is kind of two dimensional,
+  and follows the hero's journey, and poorly attemps to be amusing and #deep.
+  
+  I know I'm selling this really well.
+
+  Seeking people that have a gift with words.
+
+Projects Coming Eventually:
+
+Uniform
+  Collaborators:
+  - Phil Dragash
+  - Jess West
+
+Doorway
+  Collaborators
+  - Jake Rupp
+
+Designing Our Future
+  Collaborators:
+  - Kurt Schroeder
+  - Meg McNames
+
+Bleep Bloop:
+  Collaborators:
+  - T. L. McBeth
