84895bda53335fd77138df13580a200d721f0fa9
Felix Green <work@felix.green>
2020-03-08 17:10:58 -0400

    ♻️ abstracted PlainTextWarning into Aside

---
+++ src/components/Aside.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Hexagon from './Hexagon.svelte'
+
+  export let cx = null;
+</script>
+
+<style>
+  aside {
+    --hexagon-bg: var(--yellow-800);
+    @apply relative;
+    @apply text-orange-200;
+    @apply mx-auto;
+    @apply text-2xl;
+    @apply text-center;
+    @apply px-6;
+    @apply py-4;
+    @apply z-10;
+  }
+</style>
+
+<aside class={cx}>
+  <Hexagon style="z-index: -1;" />
+  ⬢
+  <br />
+  <slot></slot>
+  ⬢
+</aside>
