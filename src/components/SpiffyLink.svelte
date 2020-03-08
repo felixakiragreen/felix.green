fa0a7299a09ae27f6ddaa0621d94432f9cad06f0
Felix Green <work@felix.green>
2020-03-08 01:29:28 -0500

    ✨ add SpiffyLink

---
+++ src/components/SpiffyLink.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let href
+  export let target = "_self"
+  export let color 
+</script>
+
+<a {href} {target} class={`
+  p-2
+  rounded
+  text-2xl
+  text-${color}-300
+  bg-gray-800
+  hover:text-${color}-900
+  hover:bg-${color}-300
+`}>
+  &#123;<slot></slot>&#125;
+</a>
