3f6b2888fd175b1eabb6f30f3e7e5906e0d518c7
Felix Green <work@felix.green>
2020-03-11 19:04:11 -0400

    🎨 tweak padding with inline prop

--- src/components/SpiffyLink.svelte
+++ src/components/SpiffyLink.svelte
@@ -1,18 +1,20 @@
 <script>
   export let href
   export let target = "_self"
-  export let color 
+  export let color
+  export let inline = false
   export let cls = '' 
 </script>
 
 <a {href} {target} class={`
-  ${cls}
-  p-2
+  p-0
+  ${inline ? "md:px-1" : "sm:p-1 md:p-2"}
   rounded
   text-${color}-300
   bg-gray-800
   hover:text-${color}-900
   hover:bg-${color}-300
+  ${cls}
 `}>
   &#123;<slot></slot>&#125;
 </a>
