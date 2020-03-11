67ad8c837117fa33b3770f3a80302af35506e123
Felix Green <work@felix.green>
2020-03-11 19:05:24 -0400

    🎨 adding NoWrap to warning

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,8 @@
 <script>
   import Index from 'wiki/index.svelte'
   import Hexagon from 'components/Hexagon.svelte'
-  import FelixAkiraGreen from 'components/FelixAkiraGreen.svelte'
   import Aside from 'components/Aside.svelte'
+  import NoWrap from 'bits/NoWrap.svelte'
 </script>
 
 <svelte:head>
@@ -18,21 +18,23 @@
     overflow-hidden
     z-20
   `}
-  id="top"
->
+  id="top">
   <Aside>
     <p class="text-orange-300">
-    <span class="plain">
-      Welcome 🖖
-      <br />
-      Why does the text look like
-    </span>
-    <span class="text-gray-100">this</span><span class="plain">?
-      <br />
-      It's a font based on my handwriting called
-      <a href="projects#akira" class="plain text-green-300 hover:text-green-400">&#123;Akira&#125;</a>.
-    </span>
-  </p>
+      <span class="plain">
+        Welcome 🖖
+        <br />
+        Why does the text look like
+      </span>
+      <span class="text-gray-100">this</span><span class="plain">?
+        <br />
+        It's a font based on my <NoWrap>handwriting called
+        <a
+          href="projects#akira"
+          class="plain text-green-300 hover:text-green-400"
+        >Akira</a>.</NoWrap>
+      </span>
+    </p>
   </Aside>
   <Index />
 </div>
