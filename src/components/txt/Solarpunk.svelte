e0b1e0092a968b6f51906a3ffeb911669661e6df
Felix Green <work@felix.green>
2020-03-11 19:01:46 -0400

    🚚 remove old txt component

--- src/components/txt/Solarpunk.svelte
+++
@@ -1,7 +0,0 @@
-<span class="text-green-100">S</span
-><span class="text-green-200">o</span
-><span class="text-green-300">l</span
-><span class="text-green-400">a</span
-><span class="text-green-500">r</span
-><span class="text-green-600">punk</span
->
