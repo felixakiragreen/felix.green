fe1b2fb0d1b9d989283d6fbcfe497ec74ef34885
Felix Green <work@felix.green>
2020-03-12 00:05:45 -0400

    🚧 added WIP bit

---
+++ src/components/bits/WorkInProgress.svelte
@@ -0,0 +1,15 @@
+<script>
+  import SpiffyLink from 'bits/SpiffyLink.svelte'
+  import Orange from 'bits/Orange.svelte'
+  import NoWrap from 'bits/NoWrap.svelte'
+</script>
+
+<p class="-text-2 »grey plain m-4">
+  <Orange>✋TODO:</Orange>
+  I am still polishing
+  <NoWrap>✍️ copy,</NoWrap>
+  <NoWrap>📱responsiveness,</NoWrap>
+  <NoWrap>🎨 highlighting,</NoWrap>
+  and
+  <NoWrap>😀 emojis</NoWrap>.
+</p>
