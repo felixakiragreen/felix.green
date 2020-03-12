59c01ae035589467e1b3ed701fcd2aeae68974a7
Felix Green <work@felix.green>
2020-03-12 00:03:53 -0400

    ✨ add no-wrapping

--- src/components/bits/Grey.svelte
+++ src/components/bits/Grey.svelte
@@ -1,5 +1,8 @@
 <script>
   export let cls = '';
+  export let nw = false;
+  
+  let style = nw ? "white-space: nowrap;" : false
 </script>
 
-<span class="text-gray-300 {cls}"><slot></slot></span>
+<span class="text-gray-400 {cls}" {style}><slot></slot></span>
