6de77fb97480a0bdcdd1d34dd49943d851602d97
Felix Green <work@felix.green>
2020-03-12 11:00:27 -0400

    ✨ adding nw, cls props to bits

--- src/components/bits/Orange.svelte
+++ src/components/bits/Orange.svelte
@@ -1,1 +1,8 @@
-<span class="text-orange-300"><slot></slot></span>
+<script>
+  export let cls = '';
+  export let nw = false;
+  
+  let style = nw ? "white-space: nowrap;" : false
+</script>
+
+<span class="text-orange-300 {cls}" {style}><slot></slot></span>
