c280ec64a085c2a5a46f47bda3ac2e72cf00ddb9
Felix Green <work@felix.green>
2024-01-11 13:49:29 -0500

    add routes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import '@/fui/style/global.css'
+	import { stitch, getCssText } from '@/fui/ui'
+
+	const ss = stitch({
+		display: 'grid',
+		gridTemplateRows: 'auto 1fr auto',
+		minHeight: '100vh',
+		position: 'relative',
+	})
+
+	// export const ssr = false
+</script>
+
+<svelte:head>
+	<title>felix green</title>
+</svelte:head>
+
+<!-- https://discord.com/channels/752614004387610674/752795957674115092/925177257025220658 -->
+<!-- https://stackblitz.com/edit/sveltekit-1sfugv?file=src/routes/__layout.svelte -->
+<!-- for TS projects due to a bug (https://github.com/sveltejs/svelte/issues/5292)-->
+{@html `<${''}style id="stitches">${getCssText()}</${''}style>`}
+
+<main class={ss()}>
+	<div>
+		<slot />
+	</div>
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Box, Content, VStack, HStack, Text, Link } from '@/fui'
+</script>
+
+<Content bg="orange700">
+	<VStack bg="orange900">
+		<Text>Body</Text>
+	</VStack>
+</Content>
