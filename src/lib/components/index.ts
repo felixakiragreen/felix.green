08c966e7a98bfe290073f05fca823b0ac1032565
Felix Green <work@felix.green>
2024-01-12 16:48:54 -0500

    export

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,2 +1,1 @@
-export { default as Footer } from './Footer.svelte'
-export { default as Header } from './Header.svelte'
+export { default as HexagonalScrollBorder } from './HexagonalScrollBorder.svelte'
