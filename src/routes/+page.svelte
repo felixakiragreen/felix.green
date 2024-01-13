be07af7f4d11e4d326d6e8c89f8693a02455b67a
Felix Green <work@felix.green>
2024-01-13 13:42:56 -0500

    calc based on bp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,27 @@
 <Content p="0">
 	<HexagonalScrollBorder />
 	<VStack>
+		<VStack
+			fg="felix"
+			css={{
+				textAlign: 'center',
+				'@bp3': {
+					$Unit: 'calc((100vw / 6) * 1.155)',
+				},
+				'@bp6': {
+					$Unit: 'calc((100vw / 10) * 1.155)',
+				},
+				'@bp9': {
+					$Unit: 'calc((100vw / 16) * 1.155)',
+				},
+				marginTop: 'calc(var(--Unit) * 4)',
+			}}
+		>
+			<Text size="lg" css={{ opacity: 0.4 }}>
+				I<br />am</Text
+			>
+			<Text size="2xl" weight="thinn">felix green</Text>
+		</VStack>
 		<Box
 			css={{
 				width: '100px',
