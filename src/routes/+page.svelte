bda293a894fe6f2f2cc8af7b3efaaec973a753bb
Felix Green <work@felix.green>
2024-01-14 11:23:10 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,15 @@
 	import mezoHexagon from '@/assets/mezo-hexagon.svg'
 	import stretchFlatHexagon from '@/assets/stretch-flat-hexagon.svg'
 	import stretchPointyHexagon from '@/assets/stretch-pointy-hexagon.svg'
+
+	const bgSx = {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+		pointerEvents: 'none',
+	}
 </script>
 
 <Content p="0">
@@ -25,52 +34,80 @@
 			>
 			<Text size="2xl" weight="thinn">felix green</Text>
 			<VStack
-				p="0"
-				m="0"
+				mt="xl"
+				p="lg"
+				gap="lg"
 				align="center"
 				css={{
 					position: 'relative',
-					height: 'calc(var(--Unit) * 6)',
-					width: 'calc(var(--WUnit) * 6)',
-					// red with opacity
-					// backgroundColor: 'rgba(255, 0, 0, 0.2)',
+					'@bp3': {
+						width: 'calc(var(--WUnit) * 3)',
+					},
+					'@bp6': {
+						width: 'calc(var(--WUnit) * 6)',
+					},
+					'@bp9': {
+						width: 'calc(var(--WUnit) * 8)',
+					},
 				}}
 			>
 				<Box
 					css={{
-						position: 'absolute',
-						top: 0,
-						left: 0,
-						width: '100%',
-						height: '100%',
-						// backgroundColor: 'rgba(0, 0, 255, 0.2)',
+						...bgSx,
 					}}
 				>
-					<Image
-						url={mezoHexagon}
-						css={{ width: 'calc(var(--WUnit) * 6)' }}
-					/>
+					<Image url={mezoHexagon} />
 				</Box>
 				<Image url={miniHexagon} css={{ width: 'var(--HexiUnit)' }} />
-				<Image
-					url={stretchPointyHexagon}
+				<Box
 					css={{
-						width: 'var(--Unit)',
-						height: 'calc(var(--Unit) * 6)',
-						p: '$1',
+						position: 'relative',
+						px: '$xl',
+						py: '$lg',
 					}}
-				/>
+				>
+					<Box
+						css={{
+							...bgSx,
+							my: '$-2',
+							backgroundColor: '$backgroundest',
+							zIndex: 1,
+							opacity: 0.8,
+						}}
+					/>
+					<Box
+						css={{
+							...bgSx,
+							zIndex: 2,
+						}}
+					>
+						<Image
+							url={stretchFlatHexagon}
+							css={{ width: '100%', height: '100%' }}
+						/>
+					</Box>
+
+					<Text
+						color="foreground"
+						css={{ position: 'relative', zIndex: 2 }}
+						>I live for the <Text color="felix" weight="semi">future</Text
+						>. And hexagons!</Text
+					>
+				</Box>
+				<Image url={miniHexagon} css={{ width: 'var(--HexiUnit)' }} />
 				<Image
 					url={stretchFlatHexagon}
 					css={{ width: '100%', height: 'var(--Unit)', p: '$1' }}
 				/>
+				<Image url={miniHexagon} css={{ width: 'var(--HexiUnit)' }} />
 			</VStack>
 		</VStack>
 		<Box
 			css={{
 				width: '100px',
 				height: '2000px',
-				backgroundColor: 'red',
+				//  red bg with 0.2 alpha,
+				backgroundColor: 'rgba(255, 0, 0, 0.2)',
 				margin: '0 auto',
 			}}
 		></Box>
