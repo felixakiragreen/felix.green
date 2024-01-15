b54e8548c12652d361a71f7aabd458217b8606dd
Felix Green <work@felix.green>
2024-01-15 15:16:35 -0500

    add component

---
+++ src/lib/components/Socials/index.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import type { ComponentProps } from 'svelte'
+	import type { PropCss } from '@/types'
+	import { IconTheme } from '@/types'
+
+	import { socials } from './socials'
+	import { IconSocialRow, HStack } from '@/fui/bonds'
+
+	export let gap: ComponentProps<HStack>['gap'] = 'sm'
+	export let align: ComponentProps<HStack>['align'] = undefined
+	export let size: ComponentProps<IconSocialRow>['size'] = 'md'
+	export let css: PropCss = undefined
+
+	// variants
+	export let symbol: IconTheme | undefined = IconTheme.Foreground
+	export let ground: IconTheme | undefined = IconTheme.Background
+	export let hoverSymbol: IconTheme | undefined = undefined
+	export let hoverGround: IconTheme | undefined = undefined
+</script>
+
+<IconSocialRow
+	{socials}
+	{css}
+	{gap}
+	{align}
+	{size}
+	{symbol}
+	shape="hexagon"
+	look="icon"
+	{ground}
+	{hoverSymbol}
+	{hoverGround}
+/>
