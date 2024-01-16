87dd7cfc2348dd5e1f60fdbe60b9e27becceb74d
Felix Green <work@felix.green>
2024-01-16 11:10:16 -0500

    cleanup

--- src/lib/components/Socials/index.svelte
+++ src/lib/components/Socials/index.svelte
@@ -1,22 +1,17 @@
 <script lang="ts">
 	import type { ComponentProps } from 'svelte'
 	import type { PropCss } from '@/types'
+	import { HStack } from '@/fui'
 
 	import { socials } from './socials'
-	import { IconSocialRow, HStack } from '@/fui/bonds'
+	import IconSocialRow from './IconSocialRow.svelte'
 
 	export let gap: ComponentProps<HStack>['gap'] = 'sm'
 	export let align: ComponentProps<HStack>['align'] = undefined
 	export let size: ComponentProps<IconSocialRow>['size'] = 'md'
 	export let css: PropCss = undefined
+
+	export let shape: 'square' | 'hexagon' = 'square'
 </script>
 
-<IconSocialRow
-	{socials}
-	{css}
-	{gap}
-	{align}
-	{size}
-	shape="hexagon"
-	look="icon"
-/>
+<IconSocialRow {socials} {css} {gap} {align} {size} {shape} />
