113602d7528454722fa7f41aed196fd650005ef7
Felix Green <work@felix.green>
2024-01-16 10:37:21 -0500

    cleanup

--- src/fui/bonds/LinkIconSocial.svelte
+++ src/fui/bonds/LinkIconSocial.svelte
@@ -1,10 +1,5 @@
 <script lang="ts">
-	import {
-		IconTheme,
-		type PropBoolean,
-		type PropString,
-		type SocialLink,
-	} from '@/types'
+	import type { PropBoolean, PropString, SocialLink } from '@/types'
 
 	import Link from './Link.svelte'
 	import IconSocial from './IconSocial.svelte'
@@ -12,31 +7,13 @@
 	export let url: string
 	export let newTab = false
 
-	export let brandHue: PropString = undefined
-	export let brandExact: PropString = undefined
 	export let path: PropString = undefined
 	export let transform: SocialLink['transform']
 
 	export let shape: 'square' | 'hexagon' = 'square'
 	export let rounded: PropBoolean = undefined
-
-	export let symbol: IconTheme | undefined = IconTheme.Foreground
-	export let ground: IconTheme | undefined = IconTheme.Background
-	export let hoverSymbol: IconTheme | undefined = undefined
-	export let hoverGround: IconTheme | undefined = undefined
 </script>
 
 <Link {url} {newTab} look="clean">
-	<IconSocial
-		{shape}
-		{rounded}
-		{path}
-		{transform}
-		{symbol}
-		{ground}
-		{hoverSymbol}
-		{hoverGround}
-		{brandHue}
-		{brandExact}
-	/>
+	<IconSocial {shape} {rounded} {path} {transform} />
 </Link>
