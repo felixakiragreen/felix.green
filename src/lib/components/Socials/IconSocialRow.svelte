f6d516cb434bba3efcc343a4008a7183ba77a17c
Felix Green <work@felix.green>
2024-01-16 11:09:49 -0500

    mv

--- src/lib/components/Socials/IconSocialRow.svelte
+++ src/lib/components/Socials/IconSocialRow.svelte
@@ -2,16 +2,14 @@
 	import type { ComponentProps } from 'svelte'
 	import type { PropCss, SocialLink, PropBoolean } from '@/types'
 
-	import Button from './Button.svelte'
-	import HStack from './HStack.svelte'
-	import LinkIconSocial from './LinkIconSocial.svelte'
+	import { HStack, Button } from '@/fui'
+	import IconSocialLink from './IconSocialLink.svelte'
 
 	export let socials: SocialLink[]
 
 	export let gap: ComponentProps<HStack>['gap'] = 'sm'
 	export let align: ComponentProps<HStack>['align'] = undefined
 	type Size = 'sm' | 'md' | 'lg' | 'xl' | '2xl'
-	export let look: ComponentProps<Button>['look'] = undefined
 	export let size: Size | string = 'md'
 	export let css: PropCss = undefined
 
@@ -38,14 +36,12 @@
 
 <HStack css={cssInl} {gap} {align}>
 	{#each socials as { url, hex, hue, path, transform }}
-		<Button {look} size="no">
-			<LinkIconSocial
+		<Button look="icon" size="no">
+			<IconSocialLink
 				{url}
 				{path}
 				{transform}
 				{...{ shape, rounded }}
-				brandExact={hex}
-				brandHue={hue}
 				newTab={true}
 			/>
 		</Button>
