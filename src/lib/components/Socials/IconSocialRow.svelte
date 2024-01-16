9810564046fa427c80a740880930372e931449c0
Felix Green <work@felix.green>
2024-01-16 11:46:23 -0500

    cleanup

--- src/lib/components/Socials/IconSocialRow.svelte
+++ src/lib/components/Socials/IconSocialRow.svelte
@@ -9,24 +9,10 @@
 
 	export let gap: ComponentProps<HStack>['gap'] = 'sm'
 	export let align: ComponentProps<HStack>['align'] = undefined
-	type Size = 'sm' | 'md' | 'lg' | 'xl' | '2xl'
-	export let size: Size | string = 'md'
 	export let css: PropCss = undefined
 
 	export let shape: 'square' | 'hexagon' = 'square'
 	export let rounded: PropBoolean = undefined
-
-	const iconSizes: {
-		[key in string]: string
-	} = {
-		sm: '$space$5',
-		md: '$space$6',
-		lg: '$space$7',
-		xl: '$space$8',
-		'2xl': '$space$9',
-	}
-
-	$: iconSize = iconSizes[size] || size
 
 	let cssInl: PropCss
 	$: cssInl = css
