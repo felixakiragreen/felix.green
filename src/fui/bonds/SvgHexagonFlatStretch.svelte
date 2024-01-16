217a7029b195686d38792d9bc8211f9b203ed339
Felix Green <work@felix.green>
2024-01-16 12:27:58 -0500

    add

---
+++ src/fui/bonds/SvgHexagonFlatStretch.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+	import type { PropBoolean } from '@/types'
+	import { Svg } from '@/fui'
+
+	// export let viewBox = '-1.5 -1.5 42 39'
+	// export let fill = '#ebebeb'
+	// export let stroke = 'none'
+
+	// export let rounded: PropBoolean = undefined
+</script>
+
+<Svg
+	{...{
+		xmlns: 'http://www.w3.org/2000/svg',
+		'xmlns:xlink': 'http://www.w3.org/1999/xlink',
+	}}
+>
+	<symbol id="tip" viewBox="0 0 69.3 240" preserveAspectRatio="xMinYMid">
+		<path d="M69.282,0l2.718,0l0,240l-2.718,0l-69.282,-120l69.282,-120Z" />
+	</symbol>
+	<symbol id="tip-rect" viewBox="0 0 69.3 240" preserveAspectRatio="xMinYMid">
+		<rect x="0%" y="0%" width="100%" height="240" />
+	</symbol>
+
+	<mask id="mask" viewBox="0 0 240 240">
+		<rect x="0%" y="0%" width="100%" height="100%" fill="white" />
+
+		<use
+			xlink:href="#tip-rect"
+			x="0%"
+			y="0%"
+			height="100%"
+			width="50%"
+			fill="black"
+		/>
+		<use
+			xlink:href="#tip-rect"
+			x="-100%"
+			y="0%"
+			height="100%"
+			width="50%"
+			fill="black"
+			transform="scale(-1,1)"
+		/>
+	</mask>
+
+	<rect
+		mask="url(#mask)"
+		x="0%"
+		y="0%"
+		width="100%"
+		height="100%"
+		fill="var(--StretchFlatHexagonFill, #808080)"
+	/>
+
+	<use
+		xlink:href="#tip"
+		x="0%"
+		y="0%"
+		height="100%"
+		width="50%"
+		fill="var(--StretchFlatHexagonFill, #808080)"
+	/>
+	<use
+		xlink:href="#tip"
+		x="-100%"
+		y="0%"
+		height="100%"
+		width="50%"
+		fill="var(--StretchFlatHexagonFill, #808080)"
+		transform="scale(-1,1)"
+	/>
+</Svg>
