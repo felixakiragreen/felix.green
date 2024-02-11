484068a67672d480b1805cf1106a4739ed6b3df7
Felix Green <work@felix.green>
2024-02-10 19:21:44 -0500

    Create Divider.svelte

---
+++ src/lib/components/Divider.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { Box, HStack, Image } from '@/fui'
+
+	import miniHexagon from '@/assets/mini-hexagon.svg'
+</script>
+
+<HStack
+	css={{
+		width: '100%',
+	}}
+	alignV="center"
+	gap="md"
+	px="lg"
+>
+	<Box
+		css={{
+			height: '1px',
+			flex: '1',
+			bg: '$felix',
+		}}
+	/>
+	<Image
+		url={miniHexagon}
+		css={{ width: 'var(--HexiUnit)', transform: 'rotate(90deg)' }}
+	/>
+	<Box
+		css={{
+			height: '1px',
+			flex: '1',
+			bg: '$felix',
+		}}
+	/>
+</HStack>
