1fe3801a44e9b1b97c53ccc3bbf27453ea6672b7
Felix Green <work@felix.green>
2024-04-07 12:54:10 -0400

    Update include paths and extend theme

--- panda.config.ts
+++ panda.config.ts
@@ -1,18 +1,24 @@
 import { defineConfig } from '@pandacss/dev'
+import theme from './src/styles/theme'
 
 export default defineConfig({
 	// Whether to use css reset
 	preflight: true,
 
 	// Where to look for your css declarations
-	include: ['./src/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
+	include: [
+		'./src/**/*.{js,jsx,ts,tsx,astro}',
+		'./pages/**/*.{js,jsx,ts,tsx,astro}',
+	],
 
 	// Files to exclude
 	exclude: [],
 
 	// Useful for theme customization
 	theme: {
-		extend: {},
+		extend: {
+			...theme,
+		},
 	},
 
 	// The output directory for your css system
