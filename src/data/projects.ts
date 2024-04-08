6a42c545af704447683723343003908784d03da0
Felix Green <work@felix.green>
2024-04-07 23:29:25 -0400

    Add projects.ts file with initial project data

---
+++ src/data/projects.ts
@@ -0,0 +1,31 @@
+export type Project = {
+	title: string
+	description?: string
+	// logo: string
+	url?: string
+	startDate?: Date
+	endDate?: Date
+}
+
+export const ambitionProjects: Project[] = [
+	{
+		title: 'The Worm',
+		url: 'https://theworm.wtf',
+	},
+	{
+		title: 'Rooted',
+		url: 'https://rootedapp.co',
+	},
+	{
+		title: 'LifeTower',
+		url: 'https://lifetower.app',
+	},
+	{
+		title: 'Toobins',
+		// url: 'https://lifetower.app',
+	},
+	{
+		title: 'LifeTower',
+		url: 'https://lifetower.app',
+	},
+]
